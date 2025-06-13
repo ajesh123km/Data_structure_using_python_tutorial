7e284071aa2c8de30ff3e3216c0f93038ecb05c1
AJESH <158215604+ajesh123km@users.noreply.github.com>
2025-06-13 17:28:50 +0530

    Update gfg_matrix_d40_Search_in_a_sorted_Matrix.py

--- Matrix/gfg_matrix_d40_Search_in_a_sorted_Matrix.py
+++ Matrix/gfg_matrix_d40_Search_in_a_sorted_Matrix.py
@@ -17,3 +17,9 @@ def searchMatrix(mat, x):
             hi = mid - 1
 
     return False
+
+if __name__ == "__main__":
+    mat = [[1, 5, 9], [14, 20, 21], [30, 34, 43]]
+    x = 14
+
+    print("true" if searchMatrix(mat, x) else "false")
